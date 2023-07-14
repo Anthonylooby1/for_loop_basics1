03399800f1edf576348c06b24aaad399454018d3
Anthonylooby1 <anthonylooby@yahoo.com>
2023-07-14 18:01:45 -0400

    first commit

---
+++ loop_basics.py
@@ -0,0 +1,47 @@
+# def print150():
+#     for i in range(151):
+#         print(i)
+# print150()  
+
+
+
+# def print5():
+#     for i in range(5,1005,5):
+#         print(i)
+# print5()  
+
+
+
+# def getint():
+#     for x in range(0,100):
+#         if x % 10 == 0:
+#             print(x, 'Dojo')
+#         elif x % 5 == 0:
+#             print(x, 'Coding')   
+
+# getint()  
+
+# def addodd():
+#     oddcount = 0
+#     for x in range(1,500000,2):
+#         oddcount =+ x
+#         print(oddcount)
+# addodd()   
+
+
+# def count4():
+#     for x in range(2018,0,-4):
+#         print(x)
+# count4()        
+
+
+# def flexcount():
+#     lowNum = 2
+#     highNum = 9
+#     mult = 3
+
+#     for x in range(lowNum, highNum):
+#         if x % mult == 0:
+#             print(x, mult)
+
+# flexcount()
